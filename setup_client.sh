3f9d5be09845a1ada734b8e1311f6eab70c54d75
Matthew Elwin <elwin@northwestern.edu>
2021-12-18 23:50:29 -0600

    Making the client setup do everythign properly
    Made sure to escape items and set permissions

--- setup_client.sh
+++ setup_client.sh
@@ -6,29 +6,36 @@ nmconnect=$1
 # hostname of the dns server
 dnshost=$2
 
-ssh-keygen -t ed25519 -C $(hostname) -f ~/.ssh/id_robotdns
+echo "Profile: $nmconnect, Host: $dnshost"
 
-echo "Send ~/.ssh/id_robotdns.pub to your system administrator to obtain access to the dns server"
+# Delete the old key, make the new key
+rm ~/.ssh/id_robotdns ~/.ssh/id_robotdns.pub
+ssh-keygen -t ed25519 -C $(hostname) -f ~/.ssh/id_robotdns -q -N ""
 
-dnsip=$(host $serv_host | grep -v IPv6 | awk '{ print $4 }')
+echo "\nSend ~/.ssh/id_robotdns.pub to your system administrator to obtain access to the dns server"
+
+dnsip=$(host $dnshost | grep -v IPv6 | awk '{ print $4 }')
 
 nmcli con clone $nmconnect ${nmconnect}.robot
-nmcli con mod $nmconnect.robot ipv4.dns-search msr 
-nmcli con mod $nmconnect.robot ipv4.ignore-auto-dns true
-nmcli con mod $nmconnect.robot ipv4.dns $dnsip
+nmcli con mod ${nmconnect}.robot ipv4.dns-search msr 
+nmcli con mod ${nmconnect}.robot ipv4.ignore-auto-dns true
+nmcli con mod ${nmconnect}.robot ipv4.dns $dnsip
 
 
-# Create the network manager startup script
-
+# Create the network manager startup script and
+echo "Creating the network manager dispatch script:"
 cat <<EOF | sudo tee /etc/NetworkManager/dispatcher.d/21-robotdns-register.sh
 #!/bin/sh
 
-if [ "$2" = "up" ] || [ "$2" = "dhcp4-change" ]
+if [ "\$2" = "up" ] || [ "\$2" = "dhcp4-change" ]
 then
-    if [ "$CONNECTION_ID" = "${nmconnect}.robot}" ] 
+    if [ "\$CONNECTION_ID" = "${nmconnect}.robot" ] 
     then
         ssh robotdns@$dnshost
     fi
 fi
 
 EOF
+sudo chmod 500 /etc/NetworkManager/dispatcher.d/21-robotdns-register.sh
+
+echo "Complete. Use nmcli con up ${nmconnect}.robot to connect"
