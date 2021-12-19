1def220d6ebc457c6f80bc0a150cdc047f469ff6
Matthew Elwin <elwin@northwestern.edu>
2021-12-19 09:53:27 -0600

    Removed escaping.
    When the script was a heredoc, some variables needed to be escaped

--- 21-robotdns-register.sh
+++ 21-robotdns-register.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "\$2" = "up" ] || [ "\$2" = "dhcp4-change" ]
+if [ "$2" = "up" ] || [ "$2" = "dhcp4-change" ]
 then
-    if [ "\$CONNECTION_ID" = "${nmconnect}.robot" ] 
+    if [ "$CONNECTION_ID" = "${nmconnect}.robot" ] 
     then
         # Execute the ssh command as the original user
         su -c sh -c 'ssh -T -i $HOME/.ssh/id_robotdns robotdns@$dnshost' $(whoami) 
