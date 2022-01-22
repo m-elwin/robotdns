1effb6ceabc26e2c79313f423b173e10b250f6dd
Matthew Elwin <elwin@northwestern.edu>
2022-01-21 18:10:17 -0600

    Updated this as the hostname

--- 21-robotdns-register.sh
+++ 21-robotdns-register.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+echo "$(whoami)" > /tmp/foo
 . ${CONNECTION_FILENAME}.settings
 if [ "$2" = "up" ] || [ "$2" = "dhcp4-change" ]
 then
     if [ "$CONNECTION_ID" = "${nmconnect}.robot" ] 
     then
         # Execute the ssh command as the original user
-        su -c sh -c 'ssh -T -i $user_home/.ssh/id_robotdns robotdns@$dnshost' $(whoami) 
+        ssh -T -o UserKnownHostsFile=$user_home/.ssh/known_hosts -i $user_home/.ssh/id_robotdns robotdns@$dnshost
     fi
 fi
